7ebc51ab06359645fd383d594ca4aa12df8092f0
Angel Contreras Dueñas <c3po.angel@gmail.com>
2024-02-06 16:49:17 -0600

    react router dom, layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Layout from "./components/Layout";
+import NuevoCliente from "./pages/NuevoCliente";
+import Index from "./pages";
+import "./index.css";
+/* permite crear rutas por medio de un objeto */
+/* centro de la aplicacion */
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Index /> },
+      { path: "/clientes/nuevo", element: <NuevoCliente /> },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
